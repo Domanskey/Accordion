c4162e873b133e56d0cda4c0ecf58cff1580aa66
domanskey <domadam04@gmail.com>
2026-01-24 00:55:10 +0100

    feat: add Accordion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="p-7">
+      <header>
+        <h1 className="text-2xl">Frequently asked questions</h1>
+        <p className="text-sm my-2.5">Here are some of the frequently asked questions</p>
+      </header>
+      <Accordion question={'I like cookies'} answer={"yolo"}></Accordion>
+    </main>
   )
 }
 
-export default App
+function Accordion({ question, answer }) {
+
+  return (
+    <>
+      <button className="bg-neutral-950 text-md text-stone-50 p-2.5 rounded-xl w-full cursor-pointer text-left">{question}</button>
+      <p className="bg-neutral-950 text-md text-stone-50 p-2.5 rounded-xl w-full cursor-pointer text-left">{answer}</p>
+    </>
+  )
+}
